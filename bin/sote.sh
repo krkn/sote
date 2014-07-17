1fbf2c5568a905c91f33c7183755600428314fa4
leny <info@flatland.be>
2014-07-17 14:31:44 +0200

    Add some colors

--- bin/sote.sh
+++ bin/sote.sh
@@ -8,25 +8,54 @@
 ###################################
 
 sote() {
-    version=0.0.1
-    usage="
-        Usage: sote [options] [command]
 
-        Commands:
+    # sote vars
 
-            list                   Lists the paths stored by sote.
-            show <name>            Shows the path corresponding to the given name.
-            add <name> [path]      Add the path to the store with the given name. If no path is given, use current path.
-            remove <name>          Remove the path stored by sote at the given name.
-            clear                  Clear all the paths stored by sote. Ask for confirmation before acting.
-            *                      Jumps to the path corresponding to the given name.
+    local version=0.0.1
+    local store="$HOME/.sote"
 
-        Options:
+    # regular colors
 
-            -h      output usage information
-            -V      output the version number
+    local K="\033[0;30m"    # black
+    local R="\033[0;31m"    # red
+    local G="\033[0;32m"    # green
+    local Y="\033[0;33m"    # yellow
+    local B="\033[0;34m"    # blue
+    local M="\033[0;35m"    # magenta
+    local C="\033[0;36m"    # cyan
+    local W="\033[0;37m"    # white
+
+    # emphasized (bolded) colors
+
+    local EMK="\033[1;30m"
+    local EMR="\033[1;31m"
+    local EMG="\033[1;32m"
+    local EMY="\033[1;33m"
+    local EMB="\033[1;34m"
+    local EMM="\033[1;35m"
+    local EMC="\033[1;36m"
+    local EMW="\033[1;37m"
+
+    # usage text
+
+    local usage="
+    ${EMW}Usage: ${Y}sote ${G}[options] ${C}[command]
+
+    ${EMC}Commands:
+
+        ${C}list                   ${W}Lists the paths stored by sote.
+        ${C}show ${B}<name>            ${W}Shows the path corresponding to the given name.
+        ${C}add ${B}<name> ${G}[path]      ${W}Add the path to the store with the given name. If no path is given, use current path.
+        ${C}remove ${B}<name>          ${W}Remove the path stored by sote at the given name.
+        ${C}clear                  ${W}Clear all the paths stored by sote. Ask for confirmation before acting.
+        ${C}*                      ${W}Jumps to the path corresponding to the given name.
+
+    ${EMG}Options:
+
+        ${G}-h      ${W}output usage information
+        ${G}-V      ${W}output the version number
     "
-    store="$HOME/.sote"
+
 
     # Check for git
 
@@ -42,7 +71,7 @@ sote() {
     # Show help if no args
 
     if [ $# == 0 ] ; then
-        echo "$usage"
+        echo -e "$usage"
         return;
     fi
 
@@ -54,19 +83,19 @@ sote() {
 
     case "$action" in
         "-v")
-            echo "sote v$version"
+            echo -e "${Y}sote ${W}v$version"
             return;
             ;;
         "--version")
-            echo "sote v$version"
+            echo -e "${Y}sote ${W}v$version"
             return;
             ;;
         "-h")
-            echo "$usage"
+            echo -e "$usage"
             return;
             ;;
         "--help")
-            echo "$usage"
+            echo -e "$usage"
             return;
             ;;
         "list")
