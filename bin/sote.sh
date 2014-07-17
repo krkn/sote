aa69b4b9e195abdf67ce0f1221ad8fe4180e405d
leny <info@flatland.be>
2014-07-17 14:08:33 +0200

    Basic functional implementation

--- bin/sote.sh
+++ bin/sote.sh
@@ -7,92 +7,93 @@
 # Licensed under the MIT license. #
 ###################################
 
-# Globals
+sote() {
+    version=0.0.1
+    usage="
+        Usage: sote [options] [command]
 
-version=0.0.1
-usage="
-    Usage: sote [options] [command]
+        Commands:
 
-    Commands:
+            list                   Lists the paths stored by sote.
+            show <name>            Shows the path corresponding to the given name.
+            add <name> [path]      Add the path to the store with the given name. If no path is given, use current path.
+            remove <name>          Remove the path stored by sote at the given name.
+            clear                  Clear all the paths stored by sote. Ask for confirmation before acting.
+            *                      Jumps to the path corresponding to the given name.
 
-        list                   Lists the paths stored by sote.
-        show <name>            Shows the path corresponding to the given name.
-        add <name> [path]      Add the path to the store with the given name. If no path is given, use current path.
-        remove <name>          Remove the path stored by sote at the given name.
-        clear                  Clear all the paths stored by sote. Ask for confirmation before acting.
-        *                      Jumps to the path corresponding to the given name.
+        Options:
 
-    Options:
+            -h      output usage information
+            -V      output the version number
+    "
+    store="$HOME/.sote"
 
-        -h      output usage information
-        -V      output the version number
-"
-store="$HOME/.sote"
+    # Check for git
 
-# Show help if no args
+    command -v git >/dev/null 2>&1 || { echo "sote requires git but it's not installed. Aborting." >&2; return 1; }
 
-if [ $# == 0 ] ; then
-    echo "$usage"
-    exit 1;
-fi
+    # Check for store file
 
-# Options
+    if [ ! -f $store ]
+    then
+        echo "" > $store
+    fi
 
-while getopts ":vh" optname
-  do
-    case "$optname" in
-      "v")
-        echo "sote v$version"
-        exit 0;
-        ;;
-      "h")
+    # Show help if no args
+
+    if [ $# == 0 ] ; then
         echo "$usage"
-        exit 0;
-        ;;
-      "?")
-        echo "Unknown option $OPTARG"
-        exit 0;
-        ;;
-      ":")
-        echo "No argument value for option $OPTARG"
-        exit 0;
-        ;;
-      *)
-        echo "Unknown error while processing options"
-        exit 0;
-        ;;
+        return;
+    fi
+
+    # Commands
+
+    action=$1
+    arg1=$2
+    arg2=$3
+
+    case "$action" in
+        "-v")
+            echo "sote v$version"
+            return;
+            ;;
+        "--version")
+            echo "sote v$version"
+            return;
+            ;;
+        "-h")
+            echo "$usage"
+            return;
+            ;;
+        "--help")
+            echo "$usage"
+            return;
+            ;;
+        "list")
+            # TODO : parse & show
+            git config --file $store --list
+            return;
+            ;;
+        "show")
+            git config --file $store --get "store.$arg1"
+            return;
+            ;;
+        "add")
+            git config --file $store --replace-all "store.$arg1" $arg2
+            return;
+            ;;
+        "remove")
+            git config --file $store --unset-all "store.$arg1"
+            return;
+            ;;
+        "clear")
+            echo "" > $store
+            return;
+            ;;
+        *)
+            path=$( git config --file $store --get "store.$action" )
+            cd $path
+            return;
+            ;;
     esac
-  done
-
-# Commands
-
-action=$1
-arg1=$2
-arg2=$3
-
-case "$action" in
-    "list")
-        echo "list."
-        exit 0;
-        ;;
-    "show")
-        echo "show $arg1."
-        exit 0;
-        ;;
-    "add")
-        echo "add $arg1 $arg2."
-        exit 0;
-        ;;
-    "remove")
-        echo "remove $arg1."
-        exit 0;
-        ;;
-    "clear")
-        echo "clear."
-        exit 0;
-        ;;
-    *)
-        echo "jumpto $action."
-        exit 0;
-        ;;
-esac
+}
